209e431403afecbbabcff91d074ff4bb63c156b5
saymagic <saymagic.dev@gmail.com>
2016-10-30 19:16:16 +0800

    [Mod] comment style

--- algorithms/Two-Sum.js
+++ algorithms/Two-Sum.js
@@ -2,22 +2,23 @@
 //Date: 2016年10月30日
 
 /**
-    Given an array of integers, return indices of the two numbers such that they add up to a specific target.
-
-    You may assume that each input would have exactly one solution.
-
-    Example:
-    Given nums = [2, 7, 11, 15], target = 9,
-
-    Because nums[0] + nums[1] = 2 + 7 = 9,
-    return [0, 1].
+*    Given an array of integers, return indices of the two numbers such that *    they add up to a specific target.
+*
+*    You may assume that each input would have exactly one solution.
+*
+*    Example:
+*    Given nums = [2, 7, 11, 15], target = 9,
+*
+*    Because nums[0] + nums[1] = 2 + 7 = 9,
+*    return [0, 1].
 **/
 
 /**
- * @param {number[]} nums
- * @param {number} target
- * @return {number[]}
+ *   @param {number[]} nums
+ *   @param {number} target
+ *   @return {number[]}
  */
+
 var twoSum = function(nums, target) {
     var cache = new Object();
     var length = nums.length;
